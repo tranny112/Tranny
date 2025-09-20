6bac21c91cd10abe2cb95cf5aae86c99c00aa8b3
tranny112 <trannytranie@gmail.com>
2025-09-20 08:47:51 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Prof Tonney', // Your name
+  botOwner: 'ProTonney', // Your name
   ownerNumber: '254107406152', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
