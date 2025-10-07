c661d5dc29eba314da3bfa8565f7dbbc75b9b536
tranny112 <trannytranie@gmail.com>
2025-10-07 15:45:40 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "KC",
   botOwner: 'Clare', // Your name
-  ownerNumber: '+254704771738', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '+254 727 877518', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
